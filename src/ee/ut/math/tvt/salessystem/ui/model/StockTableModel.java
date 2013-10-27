9659bebb9972f7e6560a7fb87cdaedc80a6c5ad7
stenno12 <stenno.sirdla@gmail.com>
2013-10-27 21:51:47 +0200

    uus build fail

--- src/ee/ut/math/tvt/salessystem/ui/model/StockTableModel.java
+++ src/ee/ut/math/tvt/salessystem/ui/model/StockTableModel.java
@@ -1,7 +1,5 @@
 package ee.ut.math.tvt.salessystem.ui.model;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.apache.log4j.Logger;
@@ -73,7 +71,5 @@ public class StockTableModel extends SalesSystemTableModel<StockItem> {
 
 		return buffer.toString();
 	}
-	
-
 
 }
